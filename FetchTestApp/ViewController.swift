23172318ce5f42892ff8182c9c64890ff17bf051
lukemorse <luke.morse@gmail.com>
2020-01-17 15:24:37 -0600

    Initial Commit

---
+++ FetchTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FetchTestApp
+//
+//  Created by Luke Morse on 1/17/20.
+//  Copyright © 2020 Luke Morse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
